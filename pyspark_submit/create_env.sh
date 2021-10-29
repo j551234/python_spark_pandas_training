f4cd0f13bc3eaaccc742a9b9acab478365cb5c6d
JamesPeng <james850704@gmail.com>
2021-10-29 16:36:18 +0800

    add submit script

---
+++ pyspark_submit/create_env.sh
@@ -0,0 +1,35 @@
+export SPARK_HOME=/opt/cloudera/parcels/SPARK2/lib/spark2
+export HADOOP_USER_NAME=hdfs
+
+build_venv_python3(){
+    DIR="$( cd "$( dirname "$0" )" && pwd )"
+    virtualenv -p $(which python3) venv
+    python3 -m venv venv
+    . ${DIR}/venv/bin/activate
+    pip3 install -r ${DIR}/requirements.txt
+    python3 -m compileall .
+    deactivate
+}
+
+build_venv_python2(){
+    DIR="$( cd "$( dirname "$0" )" && pwd )"
+    virtualenv -p $(which python) venv
+    python -m venv venv
+    . ${DIR}/venv/bin/activate
+    pip install -r ${DIR}/requirements.txt
+    python -m compileall .
+    deactivate
+}
+
+version=$1
+case $version in
+  '2.7')
+    build_venv_python2
+    ;;
+  '3')
+    build_venv_python3
+    ;;
+  *)
+    echo "wrong input version. please give version 2.7 or 3."
+    ;;
+esac
